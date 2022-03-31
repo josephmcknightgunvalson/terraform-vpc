ce73485108ec96b9cbb11bcfa6d229fa95a2c3fa
Joseph McKnight Gunvalson <josephgunvalson@gmail.com>
2022-03-31 10:15:04 -0500

    committing

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+    type = string
+    default = "us-east-1"
+}
+
+variable "vpc_name" {
+    type = string
+}
+
+variable "cidr_block" {
+    type = string
+}
---
+++ vpc.tf
@@ -0,0 +1,24 @@
+resource "aws_vpc_ipam" "vpc" {
+  operating_regions {
+    region_name = var.region
+  }
+}
+
+resource "aws_vpc_ipam_pool" "vpc" {
+  address_family = "ipv4"
+  ipam_scope_id  = aws_vpc_ipam.vpc.private_default_scope_id
+  locale         = var.region
+}
+
+resource "aws_vpc_ipam_pool_cidr" "vpc" {
+  ipam_pool_id = aws_vpc_ipam_pool.vpc.id
+  cidr         = var.cidr_block
+}
+
+resource "aws_vpc" "vpc" {
+    ipv4_ipam_pool_id   = aws_vpc_ipam_pool.vpc.id
+    ipv4_netmask_length = 28
+    depends_on = [
+      aws_vpc_ipam_pool_cidr.vpc
+    ]
+}
